83327874381069bf311d73015e579a3c731dbe7e
Matheus <matheuscruzhen@gmail.com>
2022-08-02 18:02:46 -0300

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+import QuestionList from "./features/questions/List";
+import SingleQuestion from "./features/questions/SingleQuestion";
+import CreateForm from "./features/questions/CreateForm";
+import EditForm from "./features/questions/EditForm";
 import { store } from "./store";
 
 import "./index.css";
@@ -13,7 +17,15 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/">
+            <Route index element={<App />} />
+            <Route path="/questions">
+              <Route index element={<QuestionList />} />
+              <Route path=":id" element={<SingleQuestion />} />
+              <Route path="create" element={<CreateForm />} />
+              <Route path="edit/:id" element={<EditForm />} />
+            </Route>
+          </Route>
         </Routes>
       </Router>
     </Provider>
