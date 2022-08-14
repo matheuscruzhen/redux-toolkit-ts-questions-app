5c616b50ec8d67687458ad3cdad3af0f855cfdeb
Matheus <matheuscruzhen@gmail.com>
2022-08-14 18:04:22 -0300

    add pagination to list component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
+
+interface Props {
+  rows: number;
+  pageCount: number;
+  rowsPerPage: number;
+  setRowsPerPage: React.Dispatch<React.SetStateAction<number>>;
+  pageNumber: number;
+  handlePageClick: (page: { selected: number }) => void;
+}
+
+const index: React.FC<Props> = ({
+  handlePageClick,
+  pageCount,
+  pageNumber,
+  rows,
+  rowsPerPage,
+  setRowsPerPage,
+}: Props) => {
+  function displayRows() {
+    return (
+      <p className="mx-4">
+        <span>{rowsPerPage * pageNumber + 1}</span>-
+        <span>
+          {pageNumber === 0
+            ? rowsPerPage
+            : rowsPerPage * pageNumber + rowsPerPage > rows
+            ? rows
+            : rowsPerPage * pageNumber + rowsPerPage}
+        </span>
+        <span> de </span>
+        {rows}
+      </p>
+    );
+  }
+
+  return (
+    <div className="mt-4 flex flex-row w-full justify-end">
+      <span className="h-full flex flex-row">
+        <span>
+          <select
+            className="text-black rounded"
+            onChange={(e) => setRowsPerPage(+e.target.value)}
+          >
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+          </select>
+        </span>
+        <span>{displayRows()}</span>
+      </span>
+      <ReactPaginate
+        className="flex flex-row h-full  justify-center content-center"
+        previousLabel={<MdNavigateBefore size={"1.2rem"} />}
+        nextLabel={<MdNavigateNext size={"1.2rem"} />}
+        breakLabel={"..."}
+        pageRangeDisplayed={2}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"bg-slate-500"}
+        previousClassName={"previousBtn"}
+        nextClassName={"nextBtn"}
+        pageClassName={"mx-2"}
+        disabledClassName={"disabledBtn"}
+        activeClassName={"activeBtn"}
+        pageLinkClassName={"linkBtn"}
+      />
+    </div>
+  );
+};
+
+export default index;
--- src/features/questions/List.tsx
+++ src/features/questions/List.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Pagination from "../../components/Pagination";
 
 import { useAppDispatch, useAppSelector } from "../../common/hooks";
 import {
@@ -15,6 +16,11 @@ const List: React.FC = () => {
   const questionsStatus = useAppSelector(getQuestionsStatus);
   const error = useAppSelector(getQuestionsError);
 
+  const [pageNumber, setPageNumber] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const pagesVisited = pageNumber * rowsPerPage;
+  const pageCount = Math.ceil(questions.length / rowsPerPage);
+
   const classes = {
     container: "h-screen p-8 bg-neutral-900 text-white",
     heading: "text-2xl text-center font-semibold uppercase mb-4",
@@ -29,17 +35,23 @@ const List: React.FC = () => {
     }
   }, [questionsStatus, dispatch]);
 
+  const handlePageClick = (selectedPage: { selected: number }) => {
+    setPageNumber(selectedPage.selected);
+  };
+
   let content;
   if (questionsStatus === "succeeded") {
-    content = questions.map((question) => (
-      <Link
-        className={classes.listElement}
-        key={question._id}
-        to={`${question._id}`}
-      >
-        <span>{question.title}</span>
-      </Link>
-    ));
+    content = questions
+      .slice(pagesVisited, pagesVisited + rowsPerPage)
+      .map((question) => (
+        <Link
+          className={classes.listElement}
+          key={question._id}
+          to={`${question._id}`}
+        >
+          <span>{question.title}</span>
+        </Link>
+      ));
   } else if (questionsStatus === "loading") {
     content = <p>Loading...</p>;
   } else if (questionsStatus === "error") {
@@ -52,6 +64,14 @@ const List: React.FC = () => {
       </Link>
       <p className={classes.heading}>List Of Questions</p>
       <ul className={classes.list}>{content}</ul>
+      <Pagination
+        rows={questions.length}
+        pageCount={pageCount}
+        pageNumber={pageNumber}
+        rowsPerPage={rowsPerPage}
+        setRowsPerPage={setRowsPerPage}
+        handlePageClick={handlePageClick}
+      />
     </div>
   );
 };
